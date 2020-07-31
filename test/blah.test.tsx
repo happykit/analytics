3dab1ca44f6bdc1d639e5664e5f6f28e4cc4dbb5
Dominik Ferber <dominik.ferber@gmail.com>
2020-07-31 16:40:01 +0300

    add testing-library

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,13 @@
+import '@testing-library/jest-dom';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import { Thing } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    render(<Thing />);
+    expect(
+      screen.queryByText('the snozzberries taste like snozzberries')
+    ).toBeInTheDocument();
   });
 });
